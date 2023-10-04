bf81d8a01404d4d3d466b247a8712e5e8e518c28
yeibin02 <yeibin02@gmail.com>
2023-10-04 16:27:56 +0900

    feat : 문의하기 수정, 상품 상세 페이지 만드는 중 (정예빈)

--- js/s_contact.js
+++ js/s_contact.js
@@ -1,80 +1,3 @@
-// window.addEventListener("load", function () {
-//   // 숫자만 입력
-//   $("html").on("keydown", ".tel, .numberOnly", function (e) {
-//     console.log(e.keyCode);
-//     if (
-//       (e.keyCode >= 48 && e.keyCode <= 57) ||
-//       (e.keyCode >= 96 && e.keyCode <= 105)
-//     ) {
-//       return;
-//     } else if (e.keyCode == 8) {
-//       return;
-//     } else {
-//       e.stopPropagation();
-//       e.preventDefault();
-//     }
-//   });
-
-//   // 전화번호 입력박스
-//   function inputPhoneNumber(obj) {
-//     var number = obj.value.replace(/[^0-9]/g, "");
-//     var phone = "";
-//     if (number.length < 4) {
-//       return number;
-//     } else if (number.length < 7) {
-//       phone += number.substr(0, 3);
-//       phone += "-";
-//       phone += number.substr(3);
-//     } else if (number.length < 11) {
-//       phone += number.substr(0, 3);
-//       phone += "-";
-//       phone += number.substr(3, 3);
-//       phone += "-";
-//       phone += number.substr(6);
-//     } else {
-//       phone += number.substr(0, 3);
-//       phone += "-";
-//       phone += number.substr(3, 4);
-//       phone += "-";
-//       phone += number.substr(7);
-//     }
-//     obj.value = phone;
-//   }
-//   inputPhoneNumber()
-//   function validateInput() {
-//     const nameInput = document.getElementById("name").value;
-//     const telInput = document.getElementById("tel").value;
-//     const dateInput = document.getElementById("date").value;
-//     const pkInput = document.getElementById("pk").value;
-
-//     if (!nameInput) {
-//       alert("이름을 입력해주세요.");
-//       return false;
-//     }
-//     if (!telInput) {
-//       alert("연락처를 입력해주세요.");
-//       return false;
-//     }
-//     if (!dateInput) {
-//       alert("원하시는 상담 날짜를 입력해주세요.");
-//       return false;
-//     }
-//     if (!pkInput) {
-//       alert("문의하고 싶은 상품을 입력해주세요.");
-//       return false;
-//     }
-//     const complete = document.querySelector(".co-submit");
-//     complete.addEventListener("click", function (e) {
-//       e.preventDefault();
-//       if (validateInput) {
-//         alert("참여해주셔서 감사합니다!");
-//         offClick();
-//       }
-//     });
-//     return true;
-//   }
-//   validateInput();
-// });
 $(document).ready(function () {
   // 숫자만 입력
   $("html").on("keydown", ".tel, .numberOnly", function (e) {
@@ -123,6 +46,7 @@ $(document).ready(function () {
     const telInput = $("#tel").val();
     const dateInput = $("#date").val();
     const pkInput = $("#pk").val();
+    const agreement = $("#mand1:checked").length;
 
     if (!nameInput) {
       alert("이름을 입력해주세요.");
@@ -140,16 +64,40 @@ $(document).ready(function () {
       alert("문의하고 싶은 상품을 입력해주세요.");
       return false;
     }
+    if (!agreement) {
+      alert("약관에 동의하셔야 회원가입을 하실 수 있습니다.");
+      return false;
+    }
 
+    // 입력한 정보를 확인할 문자열 생성
+    var joinInfo =
+      "이름 : " +
+      nameInput +
+      "\n" +
+      "연락처 : " +
+      telInput +
+      "\n" +
+      "상담날짜 : " +
+      dateInput +
+      "\n" +
+      "문의하고 싶은 상품 : " +
+      pkInput +
+      "\n";
+
+    // 확인 다이얼로그 표시
+    var con = confirm(joinInfo + "입력하신 정보가 맞습니까?");
+    if (con) {
+      alert("정상적으로 문의되었습니다.");
+      // 문의가 완료되면 index.html로 이동
+      window.location.href = "index.html";
+    }
     return true;
   }
 
+  // .co-submit을 눌렀을 때 validateInput 함수 호출
   $(".co-submit").on("click", function (e) {
     e.preventDefault();
     if (validateInput()) {
-      alert("참여해주셔서 감사합니다!");
-
-      // offClick(); // offClick 함수가 어디에 정의되어 있는지 확인 필요
     }
   });
 });
