f7aeb4bf66d59f76d6647f8f84878b62a698f4ef
yeibin02 <yeibin02@gmail.com>
2023-10-05 20:32:43 +0900

    feat : 여러가지 이것저것 훙냥냥

--- js/s_package.js
+++ js/s_package.js
@@ -1,2 +1,8 @@
 $(document).ready(function () {
+  $(".ps_first_button").click(function () {
+    location.href = "s_package_buy.html";
+  });
+  $(".ps_second_button").click(function () {
+    location.href = "login.html";
+  });
 });
